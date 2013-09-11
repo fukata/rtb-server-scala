eac2a13ea806d8a0efe249b74c3e38dcf8d0eac3
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2013-09-11 11:56:00 +0900

    RTB->Rtb

--- rtbserver/src/test/scala/com/ad_stir/rtb/RtbServerServletSpec.scala
+++ rtbserver/src/test/scala/com/ad_stir/rtb/RtbServerServletSpec.scala
@@ -3,12 +3,12 @@ package com.ad_stir.rtb
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class RTBServerServletSpec extends ScalatraSpec { def is =
-  "GET / on RTBServerServlet"                     ^
+class RtbServerServletSpec extends ScalatraSpec { def is =
+  "GET / on RtbServerServlet"                     ^
     "should return status 200"                  ! root200^
                                                 end
 
-  addServlet(classOf[RTBServerServlet], "/*")
+  addServlet(classOf[RtbServerServlet], "/*")
 
   def root200 = get("/") {
     status must_== 200
